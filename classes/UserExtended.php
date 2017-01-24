fb5b0770d2b7fe4f103d1049a5986a279ef61a42
Shawn Clake <shawn.clake@gmail.com>
2017-01-24 16:37:08 -0600

    Added foundation to UserExtended extensiblity and comments.

--- classes/UserExtended.php
+++ classes/UserExtended.php
@@ -10,27 +10,67 @@ namespace Clake\UserExtended\Classes;
  *
  * Extend this class to build a module for UserExtended.
  *
+ * Usage example: Module Name: clakeForum  Function Call: getUserActivity()
+ *      Then the call would look like: UserExtended::clakeForum()->getUserActivity();
+ *
+ * In order to add your module to the module registry you must create an instance of your extensible class
+ * on run inside of your plugin's plugin.php
  *
  * @package Clake\UserExtended\Classes
  */
 abstract class UserExtended
 {
 
+    /**
+     * The name of the module
+     * Please use the syntax of authorModuleName
+     * @var null
+     */
     public $name = null;
 
+    /**
+     * The full name of the author of the plugin
+     * For example: Shawn Clake
+     * @var null
+     */
     public $author = null;
 
+    /**
+     * A brief description of the module's purpose
+     * In the case of the forum module the description might be something like:
+     *      "Provides user stats and forum content for other plugins to easily utilize"
+     * @var null
+     */
     public $description = null;
 
+    /**
+     * Whether or not other modules are able to access your modules extensible class.
+     * Typically this should always be true, the only cases where you would want to override this would be
+     * if your module doesn't provide any extra functions for other modules to use.
+     * @var bool
+     */
     public $visible = true;
 
-    public static $modules = [];
+    /**
+     * The module registry
+     * An array of all the registerd modules. This is populated at runtime.
+     * @var array
+     */
+    private static $modules = [];
 
+    /**
+     * UserExtended constructor.
+     */
     public function __construct()
     {
         $this->register();
     }
 
+    /**
+     * Creates a module record for the module registry.
+     * The module registry has the format:
+     *      [moduleName=>moduleRecord]
+     */
     public function register()
     {
         $module = new \stdClass();
@@ -42,12 +82,23 @@ abstract class UserExtended
         self::$modules[$this->name] = $module;
     }
 
+    /**
+     * Utilize a registered module
+     * The call might look like: UserExtended::clakeForum()->getUserActivity();
+     * @param $name
+     * @param $args
+     * @return bool
+     */
     public static function __callStatic($name, $args)
     {
         if(!array_key_exists($name, self::$modules))
             return false;
 
-        return self::$modules[$name]->instance;
+        $module = self::$modules[$name];
+
+        if(!$module->visible)
+
+        return $module->instance;
     }
 
 
