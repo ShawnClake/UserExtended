4786b1758a7ee2eddc8d9f509f85341b3bba4e55
Shawn Clake <shawn.clake@gmail.com>
2017-01-25 08:51:52 -0600

    Fixed bugs with the StaticFactory class.

--- classes/StaticFactory.php
+++ classes/StaticFactory.php
@@ -1,14 +1,27 @@
 <?php
 
-
 namespace Clake\UserExtended\Classes;
 
 /**
  * Class StaticFactory
+ *
+ * Used as a method of simplifying syntax whilst safely creating objects.
+ * 		An example would be for a Class called ClassD to have a funcion destroyFactory() and appendAndPrint()
+ * 			ClassD::destroy('never say never ')->appendAndPrint('not like this');
+ *
+ * Any functions inside of ClassD which you desire to be factory compatabile must have a function name that ends in 'Factory'
+ * However when utilizing the factory, don't write the word 'Factory' as you saw in the example above.
+ *
  * @package Clake\UserExtended\Classes
  */
 class StaticFactory
 {
+
+	public static function factory()
+	{
+		$class=get_called_class();
+		return new $class;
+	}
 
     /**
      * @param $name
@@ -17,9 +30,8 @@ class StaticFactory
      */
     public static function __callStatic($name, $arguments)
     {
-        $object = new static;
-        return call_user_func_array(array($object, $name), $arguments);
+        $object = self::factory();
+        return call_user_func_array(array($object, $name.'Factory'), $arguments);
     }
-
 
 }
