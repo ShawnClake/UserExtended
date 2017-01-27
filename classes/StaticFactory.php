f4807fe7678258543a496fdecbfb9959c9500133
Shawn Clake <shawn.clake@gmail.com>
2017-01-27 13:44:33 -0600

    Minor changes

--- classes/StaticFactory.php
+++ classes/StaticFactory.php
@@ -9,7 +9,7 @@ namespace Clake\UserExtended\Classes;
  * 		An example would be for a Class called ClassD to have a funcion destroyFactory() and appendAndPrint()
  * 			ClassD::destroy('never say never ')->appendAndPrint('not like this');
  *
- * Any functions inside of ClassD which you desire to be factory compatabile must have a function name that ends in 'Factory'
+ * Any functions inside of ClassD which you desire to be factory compatible must have a function name that ends in 'Factory'
  * However when utilizing the factory, don't write the word 'Factory' as you saw in the example above.
  *
  * @package Clake\UserExtended\Classes
