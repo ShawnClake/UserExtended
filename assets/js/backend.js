c16e5f3eff673b806a6572842db0d4514ec14487
Shawn Clake <shawn.clake@gmail.com>
2017-03-07 10:52:00 -0600

    Implemented a better JS obj pattern. I will slowly implement this across all of the JS as time goes.

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,5 +1,41 @@
-var UE = UE || {};
+window.UE = window.UE || {};
 
+(function (Validator, $, undefined) {
+
+    Validator.registerEvents = function() {
+        $('#name').on("keyup", function(){
+            if($(this).val() == ""){
+                $('#NameError').show();
+            } else {
+                $('#NameError').hide();
+            }
+            Validator.disableSubmit();
+        });
+
+        $('#code').on("keyup", function(){
+            if($(this).val() == "") {
+                $('#CodeError').show();
+            } else {
+                $('#CodeError').hide();
+            }
+            Validator.disableSubmit();
+        });
+    };
+
+    Validator.disableSubmit = function () {
+        if($('#name').val() == "" || $('#code').val() == "")
+        {
+            $('#ErrorBox').show();
+            $('#submitButton').attr('disabled', true);
+        } else {
+            $('#ErrorBox').hide();
+            $('#submitButton').attr('disabled', false);
+        }
+    };
+
+} (window.UE.Validator = window.UE.Validator || {}, jQuery));
+
+/*
 var Validator = (function() {
 
     var Validator = function () {
@@ -42,7 +78,7 @@ var Validator = (function() {
 })();
 
 UE.Validator = new Validator();
-
+*/
 /*
  function disableSubmit()
  {
