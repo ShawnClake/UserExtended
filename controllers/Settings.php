cb681a8f9ebaa35164a977ca0d30d86af54671fd
QuinnBast <Quinn.Bast@hotmail.com>
2017-03-14 15:56:59 -0600

    *Created a settings table *Settings saved in database *Looking at settings will show all current login fields

--- controllers/Settings.php
+++ controllers/Settings.php
@@ -13,6 +13,8 @@ use October\Rain\Support\Facades\Flash;
 use Redirect;
 use Backend;
 use Session;
+use Schema;
+use Db;
 
 /**
  * User Extended by Shawn Clake
@@ -36,6 +38,8 @@ class Settings extends Controller
 
     public $formConfig = 'config_form.yaml';
     public $listConfig = 'config_list.yaml';
+	
+	private $table = 'clake_userextended_settings';
 
     public function __construct()
     {
@@ -52,7 +56,58 @@ class Settings extends Controller
 	
 	public function start(){
 		$this->pageTitle = "Manage locations";
+		$this->checkTable();
 		//die("Hey! Listen!");
 		//$this->makePartial('init');
+	}
+	
+	public function checkTable(){
+		if(Schema::hasTable($this->table)){
+			return;
+		} else {
+			Schema::create($this->table, function ($table) {
+				$table->increments('id');
+				$table->text('name');
+				$table->boolean('required');
+				$table->integer('min');
+				$table->integer('max');
+				$table->text('validation');
+				});
+			$this->populateTable();
+		}
+	}
+	
+	public function populateTable(){
+	$table = $this->table;
+		Db::table($table)->insert(
+		['name' => 'first_name',
+		'required' => true,
+		'min' => 2,
+		'max' => 255,
+		'validation' => 'letters']);
+		
+		Db::table($table)->insert(
+		['name' => 'last_name',
+		'required' => true,
+		'min' => 2,
+		'max' => 255,
+		'validation' => 'letters']);
+		
+		Db::table($table)->insert(
+		['name' => 'email',
+		'required' => true,
+		'min' => 2,
+		'max' => 255,
+		'validation' => 'email']);
+		
+		Db::table($table)->insert(
+		['name' => 'password',
+		'required' => true,
+		'min' => 8,
+		'max' => 255]);
+	}
+	
+	public function getSettings(){
+		return Db::table($this->table)->select('*')->get();
 	}
 }
